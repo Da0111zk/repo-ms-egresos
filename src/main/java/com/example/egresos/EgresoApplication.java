6ac0439a06cdc9c34a372e8555695fc65c3798d6
Da0111zk <da.azocard@duocuc.cl>
2026-06-30 18:09:28 -0400

    Fix: Dockerfiles y Application.yaml

--- src/main/java/com/example/egresos/EgresoApplication.java
+++ src/main/java/com/example/egresos/EgresoApplication.java
@@ -2,8 +2,10 @@ package com.example.egresos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EgresoApplication {
     public static void main(String[] args) {
         SpringApplication.run(EgresoApplication.class, args);
